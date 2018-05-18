43a81907c5f24cd8c919465a70f62b4a464e1877
chenyw <chen_yw@126.com>
2018-05-18 18:12:48 +0800

    lnmp

--- app/index.php
+++ app/index.php
@@ -1,2 +1,16 @@
 <?php
-echo 'i am PHP';
+// phpinfo();
+
+// 建立连接
+try{
+    $dbh = new PDO("mysql:host=172.20.0.2;dbname=mysql", "root", 123456);
+    $dbh->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+    $dbh->exec("SET CHARACTER SET utf8");
+    $dbh=null; //断开连接   
+}catch(PDOException $e){
+    print"Error!:".$e->getMessage()."<br/>";
+    die();
+}
+// 错误检查S
+// 输出成功连接
+echo "<h1>成功通过 PDO 连接 MySQL 服务器</h1>" . PHP_EOL;
