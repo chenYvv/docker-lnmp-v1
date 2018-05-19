e6d25ad3880eb5cbf03a9164ef3cc522d17f5765
chenyw <chen_yw@126.com>
2018-05-19 18:02:19 +0800

    + redis

--- app/index.php
+++ app/index.php
@@ -1,7 +1,6 @@
 <?php
-// phpinfo();
 
-// 建立连接
+// 测试mysql连接
 try{
     $dbh = new PDO("mysql:host=mysql;dbname=mysql", "root", 123456);
     $dbh->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
@@ -11,6 +10,26 @@ try{
     print"Error!:".$e->getMessage()."<br/>";
     die();
 }
-// 错误检查S
-// 输出成功连接
-echo "<h1>成功通过 PDO 连接 MySQL 服务器</h1>" . PHP_EOL;
+
+echo "<h1 style='text-align:center;color:green'>PDO MySQL 连接成功</h1>" . PHP_EOL;
+
+
+// 测试redis连接
+try{
+    $redis = new redis();
+    $redis->connect('redis','6379');
+    // 设置一个字符串的值
+    $redis->set('key', 'i am value');
+
+    //获取一个字符串的值
+    // echo $redis->get('key');
+
+}catch(PDOException $e){
+    print"Error!:".$e->getMessage()."<br/>";
+    die();
+}
+
+echo "<h1 style='text-align:center;color:green'>redis 连接成功</h1>" . PHP_EOL;
+
+
+phpinfo();
